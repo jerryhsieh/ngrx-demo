4bedf96d6b39ddc98837a1b14d3cca4f8d18db75
Jerry Hsieh <jerryhsieh01@gmail.com>
2018-01-01 15:36:34 +0800

    add navbar test spec

--- src/app/navbar/navbar.component.spec.ts
+++ src/app/navbar/navbar.component.spec.ts
@@ -1,4 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { Observable } from 'rxjs/Observable';
 
 import { ShareModule } from '../share/share.module';
 import { NavbarComponent } from './navbar.component';
@@ -6,6 +9,7 @@ import { NavbarComponent } from './navbar.component';
 describe('NavbarComponent', () => {
     let component: NavbarComponent;
     let fixture: ComponentFixture<NavbarComponent>;
+    let toolbar: DebugElement;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
@@ -19,11 +23,20 @@ describe('NavbarComponent', () => {
         fixture = TestBed.createComponent(NavbarComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
+        toolbar = fixture.debugElement.query(By.css('mat-toolbar'));
     });
 
     it('should create', () => {
         expect(component).toBeTruthy();
     });
 
+    it('should change background', async(() => {
+        component.login$ = Observable.of(true);
+        fixture.detectChanges();
+        expect(toolbar.styles.background).toBe('darkviolet');
 
+        component.login$ = Observable.of(false);
+        fixture.detectChanges();
+        expect(toolbar.styles.background).toBe('seagreen');
+    }));
 });
