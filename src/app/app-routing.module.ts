8ad7c4ed3798c6ffb4a2decad29b9bf1c2c08d22
Jerry Hsieh <jerryhsieh01@gmail.com>
2018-01-12 09:15:52 +0800

    disable enabletracing for routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -26,7 +26,7 @@ const routes: Routes = [{
 @NgModule({
     imports: [RouterModule.forRoot(
         routes,
-        { enableTracing: true }
+        //{ enableTracing: true }
     )],
     exports: [RouterModule]
 })
